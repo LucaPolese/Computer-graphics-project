4d95149700d31d6a083af2602b9dcdf3081c3ebe
Luca Polese <luca.polese@studio.unibo.it>
2024-08-25 15:31:57 +0200

    New updates to solve skybox issues

--- js/index.js
+++ js/index.js
@@ -38,29 +38,42 @@ async function main() {
 
     const meshProgramInfo = webglUtils.createProgramInfo(gl, [vertexShader, fragmentShader]);
 
-    const assets = [
-        './resources/assets/911/911.obj',
-        './resources/assets/banner/Banner.obj',
-        './resources/assets/firehydrant/FireHydrant.obj',
-        './resources/assets/gasstation/GasStation.obj',
-        './resources/assets/motel/Motel.obj',
-        './resources/assets/parkinglot/ParkingLot.obj',
-        './resources/assets/road/Road.obj',
-        './resources/assets/sign/Sign.obj',
-        './resources/assets/transporter/Transporter.obj',
-        './resources/assets/tree/Trees.obj',
-        './resources/assets/bus/Bus.obj'
-    ];
+    const obj_911 = './resources/assets/911/911.obj';
+    var model_911 = await loadObj(gl, obj_911);
 
-    // Simulate loading all models
-    const models = await Promise.all(assets.map(async url => {
-        const model = await loadObj(gl, url);
-        return model;
-    }));
+    const obj_banner = './resources/assets/banner/Banner.obj';
+    var model_banner = await loadObj(gl, obj_banner);
+
+    const obj_firehydrant = './resources/assets/firehydrant/FireHydrant.obj';
+    var model_firehydrant = await loadObj(gl, obj_firehydrant);
+
+    const obj_gasstation = './resources/assets/gasstation/GasStation.obj';
+    var model_gasstation = await loadObj(gl, obj_gasstation);
+
+    const obj_motel = './resources/assets/motel/Motel.obj';
+    var model_motel = await loadObj(gl, obj_motel);
+
+    const obj_parkinglot = './resources/assets/parkinglot/ParkingLot.obj';
+    var model_parkinglot = await loadObj(gl, obj_parkinglot);
+
+    const obj_road = './resources/assets/road/Road.obj';
+    var model_road = await loadObj(gl, obj_road);
+
+    const obj_sign = './resources/assets/sign/Sign.obj';
+    var model_sign = await loadObj(gl, obj_sign);
+
+    const obj_transporter = './resources/assets/transporter/Transporter.obj';
+    var model_transporter = await loadObj(gl, obj_transporter);
+
+    const obj_tree = './resources/assets/tree/Trees.obj';
+    var model_tree = await loadObj(gl, obj_tree);
+
+    const obj_bus = './resources/assets/bus/Bus.obj';
+    var model_bus = await loadObj(gl, obj_bus);
 
     hideLoadingModal();
 
-    function render() {
+    function render(time) {
         updateCameraPosition();
 
         webglUtils.resizeCanvasToDisplaySize(gl.canvas);
@@ -102,14 +115,23 @@ async function main() {
                 webglUtils.setBuffersAndAttributes(gl, meshProgramInfo, bufferInfo);
                 webglUtils.setUniforms(meshProgramInfo, { 
                     u_world,
-                    u_color: [material.diffuse[0], material.diffuse[1], material.diffuse[2], material.opacity]
                 }, material);
                 webglUtils.drawBufferInfo(gl, bufferInfo);
             }
         }
 
         // Assume models are loaded and ready to render
-        models.forEach(renderModel);
+        renderModel(model_911);
+        renderModel(model_banner);
+        renderModel(model_firehydrant);
+        renderModel(model_gasstation);
+        renderModel(model_motel);
+        renderModel(model_parkinglot);
+        renderModel(model_road);
+        renderModel(model_sign);
+        renderModel(model_transporter);
+        renderModel(model_tree);
+        renderModel(model_bus);
 
         requestAnimationFrame(render); // Continue the render loop
     }
